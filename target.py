d2e647892d7198a11082b38f355b3ade7a6ec414
lerafff <93663558+lerafff@users.noreply.github.com>
2021-11-04 11:40:05 +0200

    Game is over

--- target.py
+++ target.py
@@ -1,2 +1,9 @@
-print("hello, bitch")
-print("FUCK ROMANIUK")
+def sieve_flavius(num):
+    result = [1]
+    numbers= []
+    for i in range(num):
+        numbers.append(i)
+    # for i in range(3, num + 1):
+        
+    return numbers
+print(sieve_flavius(9))
